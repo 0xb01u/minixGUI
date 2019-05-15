d14c81beaf7cce9e921dd44aafabe603a746204d
Bolu <boluelbot@gmail.com>
2019-05-15 17:04:53 +0200

    Added read.sh

---
+++ read.sh
@@ -0,0 +1,7 @@
+dosdir -r fd1 > fd1.txt
+dosread fd1 MINIXGUI/GUI.C > /root/GUI/GUI.c
+dosread fd1 MINIXGUI/FTOOLS.C > /root/GUI/ftools.c
+dosread fd1 MINIXGUI/MAKEFILE > /root/GUI/Makefile
+dosread fd1 MINIXGUI/INCLUDE/COLORS.H > /root/GUI/include/colors.h
+dosread fd1 MINIXGUI/INCLUDE/FTOOLS.H > /root/GUI/include/ftools.h
+dosread fd1 MINIXGUI/INCLUDE/UBDEV.H > /root/GUI/include/ubdev.h
