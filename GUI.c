7d92e53bb83eb0e702b4c86b589aaee755b16726
Bolu <boluelbot@gmail.com>
2019-05-23 09:41:13 +0200

    Added explanations

--- GUI.c
+++ GUI.c
@@ -30,36 +30,32 @@ char getkey();
 
 int main(void)
 {
+  /*
+   * Fichero que guarda los nombres de los ficheros del directorio.
+   */
   FILE *fp;
 
-  char **items;
-  /* cat: 0123456789ABCDEF: Is a directory */
-  char *checkDirectory;
-  int n_items;
-  int limit;
-  int read = 0;
+  char **items; /* Array de nombres de los ficheros */
+  int n_items;  /* Numero de ficheros del directorio */
+  int limit;    /* Numero de ficheros por fila a imprimir */
+  int columns;  /* Número de columnas de la interfaz */
 
-  const char *options[NOPTIONS];
+  const char *options[NOPTIONS];  /* Array de nombres de las opciones */
 
-  int mode = FILES;
-  int fSelected = 0;
-  int oSelected = 0;
-  int *selected = &fSelected;
-  char action;
+  int mode = FILES;   /* Modo de ejecucion de la GUI */
+  int fSelected = 0;  /* Fichero seleccionado actualmente */
+  int oSelected = 0;  /* Opcion seleccionada actualmente */
+  int *selected = &fSelected; /* Puntero a la variable de selección
+                                correspondiente al modo actual */
+  char action;  /* Tecla que ha pulsado el usuario */
 
+  /* Buffers para almacenar Strings y poder concatenarlas */
   char *src1;
   char *src2;
-
-  int columns;
   
-  int i;
-
-  if ((checkDirectory = (char*) malloc(38 * sizeof (char))) == NULL)
-  {
-      printf("Error al reservar memoria para la interfaz\n");
-      exit(-1);
-  }
+  int i;  /* Variable para iterar en bucles */
   
+  /* Opciones a imprimir en la primera fila: */
   options[0] = "Parent";
   options[1] = "Console";
   options[2] = "Shut down";
@@ -67,8 +63,12 @@ int main(void)
   options[4] = "Recompile";
   /* "Parent   Console   Shut down   Reboot   Recompile" */
 
+  /* Bucle principal: */
   while (1)
   {
+    /* Alocación de la memoria de los buffers de Strings.
+       (El tamaño es fijo, pero no se puede usar arrays porque
+       da un error en otro punto del programa.) */
     if ((src1 = (char*) malloc(SOURCE_SIZE * sizeof (char))) == NULL
      || (src2 = (char*) malloc(SOURCE_SIZE * sizeof (char))) == NULL)
     {
@@ -77,13 +77,14 @@ int main(void)
     }
 
     system(CLR);
-    system("ls >" FNAME);
+    system("ls >" FNAME); /* Creación del fichero de nombres */
 
     fp = fopen(FNAME, "r");
 
     n_items = countLines(fp);
     limit = mode == FILES ? n_items : NOPTIONS;
 
+    /* Alocacion de la memoria del buffer de nombres */
     if ((items = (char**) malloc(n_items  *sizeof (char*))) == NULL)
     {
       printf("Error al reservar memoria para la lista de ficheros\n");
@@ -92,6 +93,7 @@ int main(void)
 
     for (i = 0; i < n_items; i++)
     {
+      /* Alocacion de memoria para cada nombre del buffer */
       if ((items[i] = (char*) malloc(NAME_SIZE  *sizeof (char))) == NULL)
       {
         printf("Error al reservar memoria para el nombre de los ficheros\n");
@@ -99,12 +101,15 @@ int main(void)
       }
     }
 
+    /* Almacenamiento de cada nombre en el buffer
+       (los ficheros que empiezan por '.' se ocultan). */
     i = 0;
-    while ((read = getStringLine(&items[i], fp)) != -1)
+    while (getStringLine(&items[i], fp) != -1)
       if (items[i][0] != '.') i++;
 
     fclose(fp);
 
+    /* Impresión de opciones, path, ficheros del directorio e indicaciones: */
     for (i = 0; i < NOPTIONS; i++)
     {
       if ((mode == OPTIONS) && (oSelected == i))
@@ -126,25 +131,26 @@ int main(void)
     } 
     printf(YELLOW"\n\nArrows: move   o/a: open/action   r: remove   p: go to parent\nq: options/files   t: console"WHITE"\n\n");
 
-    system("rm "FNAME);
+    system("rm "FNAME); /* Eliminamos el fichero de nombres */
 
 
     
-    action = getkey();
+    action = getkey();  /* Capturamos la tecla del usuario */
 
-    if (action == 'q')
+    if (action == 'q')  /* Cambiar de modo */
     {
       mode = -~mode % 2;
       selected = mode == FILES ? &fSelected : &oSelected;
     }
 
-    else if (action == 'p')
+    else if (action == 'p') /* Ir al directorio padre */
     {
       chdir("..");
       fSelected = 0;
     }
 
     else if (action == LEFT || action == RIGHT || action == UP || action == DOWN)
+    /* Nos movemos entre la lista de ficheros u opciones */
     {
       columns = limit < NOPTIONS ? limit : NOPTIONS;
 
@@ -177,12 +183,13 @@ int main(void)
 
     else if (mode == FILES)
     {
-      if (action == 'o' || action == 'a')
+      if (action == 'o' || action == 'a') /* Abrir un fichero o directorio */
       {
         system(CLR);
 
         if (chdir(items[fSelected]) == -1)
         {
+          /* El fichero no es un directorio */
           strcpy(src1, "cat ");
           system(strcat(src1, items[fSelected]));
 
@@ -195,7 +202,7 @@ int main(void)
         }
       }
 
-      else if (action == 'r')
+      else if (action == 'r') /* Eliminar el fichero */
       {
         printf(WHITE"\nEliminar %s? (Y/n) ", items[fSelected]);
         fflush(stdout);
@@ -207,7 +214,7 @@ int main(void)
       }
     }
 
-    if (action == 't')
+    if (action == 't')  /* Abrir consola */
     {
       mode = OPTIONS;
       action = 'o';
@@ -216,15 +223,16 @@ int main(void)
 
     if (mode == OPTIONS)
     {
-      if (action == 'o' || action == 'a')
+      if (action == 'o' || action == 'a') /* Ejecutar opcion  */
       {
         switch (oSelected)
         {
-          case 0: chdir("..");
+          case 0: /* Ir al directorio padre */
+            chdir("..");
             mode = FILES;
             fSelected = 0;
             break;
-          case 1:
+          case 1: /* Abrir consola */
             printf("\n"GREEN"MINIX console"WHITE":"BLUE"%s"WHITE"$ ", src2);
             fgets(src1, SOURCE_SIZE - 1, stdin);
 
@@ -243,22 +251,25 @@ int main(void)
             else system(src1);
             getchar();
             break;
-          case 2: system("shutdown -h now");
+          case 2: system("shutdown -h now");  /* Apagar el sistema */
             break;
-          case 3: system("reboot");
+          case 3: system("reboot"); /* Reiniciar el sistema */
             break;
-          case 4: system(COMPILE);
+          case 4: system(COMPILE);  /* Recompilar el núcleo */
             break;
           default: printf("Error al ejecutar opción");
         }
       }
     }
 
+    /* Liberamos memoria que vamos a reutilizar
+       (si no, se llena la memoria del sistema) */
     free(items);
     free(src1);
     free(src2);
 
   }
+  /* Borrar pantalla */
   system(CLR);
 
   return 0;
@@ -268,15 +279,22 @@ char getkey()
 {
   char c;
   message m;
+
+  /* Nos aseguramos de que el valor de tty_intail es 0: */
   _taskcall(MM, DELBUF, &m);
+  /* Devolvemos el valor de tty_intail hasta que no sea 0 (se ha pulsado una tecla) */
   while ((c = m.m1_i1) == 0) _taskcall(MM, GETKEY, &m);
+  /* Descartamos que la terminal use la tecla pulsada y el '\n' introducido */ 
   getchar();
   getchar();
-  if ((c & 0x80) && (c & 0x0F))
+
+  if ((c & 0x80) && (c & 0x0F)) /* Se ha pulsado una flecha */
   {
+    /* Descartamos los caracteres extra introducidos */
     getchar();
     getchar();
   }
+
   return c;
 }
 
